58ff28ce4c6d69a9a4b53052fa4eadfd257d1009
Caio <caiossfer2@gmail.com>
2022-07-14 09:19:56 -0300

    structs iniciais

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <list>
 
 using namespace std;
 
+typedef struct Palavras{
+    // 32 bits
+};
+
+typedef struct Bloco{
+    // 4 palavras de 32 bits
+};
+
+typedef struct MemoriaDados{
+    // 1024 palavras de 32 bits
+};
+
+typedef struct Cache{
+    //64 blocos. cada bloco com 4 palavras de 32 bits
+};
 
 int main(){
 
